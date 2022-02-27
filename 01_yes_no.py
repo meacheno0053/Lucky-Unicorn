319d6e5a14b36cf986d28d8a8b4f2067c188a6f0
meacheno0053 <99371795+meacheno0053@users.noreply.github.com>
2022-02-27 21:53:14 +0000

    made the yes / no checker more efficient

--- 01_yes_no.py
+++ 01_yes_no.py
@@ -1,21 +1,22 @@
-# Ask the user if they have played before
-show_instructions = input("have you played before?").lower()
 
-# If they say yes, output 'program continues'
-if show_instructions == "yes":
-  print("program continues")
+show_instructions = ""
+while show_instructions.lower() != "xxx":
+  # Ask the user if they have played before
+  show_instructions = input("have you played before?").lower()
 
-elif show_instructions == "y":
-  print("program continues")
-
-elif show_instructions == "no":
-  print("display instructions")
-
-elif show_instructions == "n" :
-  print("display instructions")
-  
+  # If they say yes, output 'program continues' 
   # If they say no, output 'display instructions'
-else:
-  print("Please answer yes / no") 
+  #if the answer is invalid, print an error
+  
+  if show_instructions == "yes" or show_instructions == "y": 
+    show_instructions = "yes"
+    print("program continues")
+  
+  elif show_instructions == "no" or show_instructions == "n": 
+    show_instructions = "no"
+    print("display instructions")
+  
+  else:
+    print("Please answer yes / no")
 
 print("you chose {}".format(show_instructions))
