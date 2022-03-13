dd99e2f5c65a6240f8849d9f39007fff4c4f79f4
meacheno0053 <99371795+meacheno0053@users.noreply.github.com>
2022-03-13 22:00:17 +0000

    combined my code into my Base and created a successful working code

--- 00_LU_Base_v_01.py
+++ 00_LU_Base_v_01.py
@@ -5,7 +5,7 @@ def yes_no (question):
     response = input(question).lower()
     
     if response == "yes" or response == "y": 
-      print("you chose yes")
+      # print("you chose yes")
       return "yes"
     
     elif response == "no" or response == "n": 
@@ -13,6 +13,7 @@ def yes_no (question):
     
     else:
       print("Please answer yes / no")
+
    
 
 def instructions ():
@@ -21,11 +22,37 @@ def instructions ():
   print("the rules of the game go here")
   print()
   return ""
+
+# checks for a number between low and high
+def num_check(question, low, high ):
+  error = "Please enter an whole number between 1 and 10\n"
+  
+  valid = False
+  while not valid: 
+    try:
+      # ask the question 
+      response = int(input(question))
+    
+      # if the amount is too low / too high give
+      if low < response <= high:
+        return response
+    
+    # output an error
+      else: 
+        print(error)
+  
+    except ValueError:
+      print(error)
       
-# Main Routine goes here
-played_before = yes_no("have you played before?")
+
+# ******* Main routine starts here ******
+played_before = yes_no("have you played before? ")
 
 if played_before == "no":
   instructions()
 
-print("program continues")
+# print("program continues")
+
+how_much = num_check ("how much would you like to play with? " , 0, 10 )
+
+print("you will be spending${}".format (how_much))
