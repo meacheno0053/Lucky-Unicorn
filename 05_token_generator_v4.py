c2627fd9a9a410bb4006334393c039e5d5cfdc36
meacheno0053 <99371795+meacheno0053@users.noreply.github.com>
2022-03-17 00:19:33 +0000

    created token generator v4

---
+++ 05_token_generator_v4.py
@@ -0,0 +1,27 @@
+import random
+
+# main routine goes here
+
+STARTING_BAlANCE = 100
+
+balance = STARTING_BAlANCE
+# Testing loop to generate 20 tokens 
+for item in range (0, 100):
+  chosen_num = random.randint (1, 100)
+  
+  # Adjust balance 
+  if 1 <= chosen_num <= 5:
+    chosen = "unicorn"
+    balance += 4
+  elif 6 <= chosen_num <= 36:
+    chosen = "donkey"
+    balance -= 1
+  else:
+    chosen = "horse / zebra"
+    balance -= 0.5
+
+
+
+print()
+print("starting balance: ${:.2f}".format(STARTING_BAlANCE))
+print ("final balance: ${:.2f}".format(balance))
--- sandbox.py
+++ sandbox.py
@@ -1,1 +1,6 @@
-print("hello world")
+import random
+
+for item in range (0, 20):
+  number = random.randint(1
+                          , 4)
+  print (number, end="\t")
