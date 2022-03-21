6289e7bf06256538ea17dc7fc068111713310767
meacheno0053 <99371795+meacheno0053@users.noreply.github.com>
2022-03-21 21:00:05 +0000

    finished v4 token generator and created 06_rounds

--- 05_token_generator_v4.py
+++ 05_token_generator_v4.py
@@ -9,16 +9,26 @@ balance = STARTING_BAlANCE
 for item in range (0, 10):
   chosen_num = random.randint (1, 100)
   
-  # Adjust balance 
+  # Adjust balance
+  # if the random # is between 1 and 5,
+  # user gets a unicorn (add $4 to balance)
   if 1 <= chosen_num <= 5:
     chosen = "unicorn"
     balance += 4
+    
+    # if the random # is between 6 and 36
+    # user gets a donkey (subtract $1 from balance)
   elif 6 <= chosen_num <= 36:
     chosen = "donkey"
     balance -= 1
+
+    # the token is either horse or zebra
+    # in both cases, subtract $0.50 from the balance
   else:
     if chosen_num % 2 ==0:
       chosen = "horse"
+
+      #otherwise set it to a zebra
     else:
       chosen = "zebra"
     balance -= 0.5
@@ -30,4 +40,3 @@ print()
 print("starting balance: ${:.2f}".format(STARTING_BAlANCE))
 print ("final balance: ${:.2f}".format(balance))
 
-print("hello world")
---
+++ 06_Rounds_v01.py
@@ -0,0 +1,22 @@
+# set balance for testing purpose
+balance = 5
+
+rounds_played = 0
+
+play_again = input ("press <Enter> to play...")
+while play_again == "":
+
+    #increase # of rounds played
+  rounds_played += 1
+
+  # print round number
+  print(rounds_played)
+  balance -= 1 
+  print("balance: ", balance)
+  print()
+  
+  play_again = input ("press enter to play again " 
+                     "or 'xxx' to quit")
+
+print()
+print("final_balance" balance )
