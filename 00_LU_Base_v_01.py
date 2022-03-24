66ee97b89fe87abf69ca9141aea29bf9d2cda05d
meacheno0053 <99371795+meacheno0053@users.noreply.github.com>
2022-03-24 00:19:55 +0000

    took finished code from token_generator_02.py and added it into the LU_Base Code is sucessful

--- 00_LU_Base_v_01.py
+++ 00_LU_Base_v_01.py
@@ -1,3 +1,4 @@
+import random
 
 def yes_no (question):
   valid = False
@@ -19,7 +20,7 @@ def yes_no (question):
 def instructions ():
   print("**** How to Play ****")
   print()
-  print("the rules of the game go here")
+  print("The rules of the game go here")
   print()
   return ""
 
@@ -46,13 +47,66 @@ def num_check(question, low, high ):
       
 
 # ******* Main routine starts here ******
-played_before = yes_no("have you played before? ")
+played_before = yes_no("Have you played before? ")
 
 if played_before == "no":
   instructions()
 
 # print("program continues")
 
-how_much = num_check ("how much would you like to play with? " , 0, 10 )
+how_much = num_check ("How much would you like to play with? " , 0, 10 )
 
-print("you will be spending${}".format (how_much))
+print("You will be spending ${}".format (how_much))
+
+balance = how_much
+
+rounds_played = 0
+
+play_again = ""
+while play_again == "":
+
+  # play_again = input ("press <Enter> to play...").lower()
+
+    #increase # of rounds played
+  rounds_played += 1
+
+  # print round number
+  print()
+  print("*** Round #{} ***".format(rounds_played))
+  chosen_num = random.randint (1, 100)
+  
+  # Adjust balance
+  # if the random # is between 1 and 5,
+  # user gets a unicorn (add $4 to balance)
+  if 1 <= chosen_num <= 5:
+    chosen = "Unicorn"
+    balance += 4
+    
+    # if the random # is between 6 and 36
+    # user gets a donkey (subtract $1 from balance)
+  elif 6 <= chosen_num <= 36:
+    chosen = "Donkey"
+    balance -= 1
+
+    # the token is either horse or zebra
+    # in both cases, subtract $0.50 from the balance
+  else:
+    if chosen_num % 2 ==0:
+      chosen = "Horse"
+
+      #otherwise set it to a zebra
+    else:
+      chosen = "Zebra"
+    balance -= 0.5
+
+  print("You got a {}. Your balance is ${:.2f}".format(chosen, balance))
+
+  if balance < 1:
+    play_again = "xxx"
+    print("Sorry you have run out of money")
+  else:
+    play_again = input(" Press Enter to play again "
+                      "or 'xxx' to quit")
+
+print()
+print("Final_balance ", balance)
--- 06_Rounds_v01.py
+++ 06_Rounds_v01.py
@@ -3,20 +3,25 @@ balance = 5
 
 rounds_played = 0
 
-play_again = input ("press <Enter> to play...")
+play_again = input ("press <Enter> to play...").lower()
 while play_again == "":
 
     #increase # of rounds played
   rounds_played += 1
 
   # print round number
-  print(rounds_played)
+  print()
+  print("*** Round #{} ***".format(rounds_played))
   balance -= 1 
   print("balance: ", balance)
   print()
-  
-  play_again = input ("press enter to play again " 
-                     "or 'xxx' to quit")
+
+if balance < 1:
+  play_again = "xxx"
+  print("Sorry you have run out of money")
+else:
+  play_again = input(" Press Enter to play again"
+                    "or 'xxx' to quit")
 
 print()
 print("final_balance", balance)
